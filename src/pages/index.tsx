a5ead6054e1374340189df43cf2a720044109729
CHOIMINSEOK <cms3718@gmail.com>
2020-03-15 01:20:50 +0900

    Main Header Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
 import React from "react"
-import { Router, Link } from "@reach/router"
-import Main from "./main"
-import LifeArchive from "./life"
+import styled from "styled-components"
 
 export const routes = {
   LIFE: "/life",
   DEV: "/dev",
 }
 
-export default () => <Main />
+export default () => (
+  <Wrapper>
+    <Title>
+      최민석
+      <Subtitle>/ Minseok Choi</Subtitle>
+    </Title>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-contents: center;
+  padding-top: 20px;
+  font-family: "Apple SD Gothic Neo", "Helvetica", "Arial";
+`
+
+const Title = styled.h1`
+  font-weight: 10;
+  font-size: 50px;
+  color: #555555;
+  display: flex;
+  flex-direction: row;
+`
+
+const Subtitle = styled.div`
+  font-weight: 10;
+  font-size: 30px;
+  color: #999999;
+  margin-left: 12px;
+  margin-bottom: 2px;
+  align-self: flex-end;
+`
